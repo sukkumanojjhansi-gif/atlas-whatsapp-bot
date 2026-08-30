14e75cc1502d8683b7ec89f291f702951e019ef1
sukkumanojjhansi-gif <sukkumanojjhansi@gmail.com>
2026-08-30 14:01:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,6 +41,6 @@ def whatsapp_webhook():
     msg.body(reply)
     return str(resp)
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
